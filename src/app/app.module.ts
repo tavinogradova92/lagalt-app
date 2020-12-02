d158e614f812aab51cbc8c0a8c54db4242dd22d4
tavinogradova92 <tavinogradova92@gmail.com>
2020-12-02 14:14:50 +0100

    New component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { NotFoundPageComponent } from './error-handling/not-found-page/not-found-page.component';
+import { TopIndustriesComponent } from './components/main-page/top-industries/top-industries.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    NotFoundPageComponent
+    NotFoundPageComponent,
+    TopIndustriesComponent
   ],
   imports: [
     BrowserModule,
