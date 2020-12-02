8f22b9f9e11bcfd5980a7f9ab5bfb9270def0f86
tavinogradova92 <tavinogradova92@gmail.com>
2020-12-02 13:27:52 +0100

    Router added to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { NotFoundPageComponent } from './error-handling/not-found-page/not-found-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
