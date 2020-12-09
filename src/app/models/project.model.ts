34b9bc7f003bbd7a5288f4345fe6b7f4eb88d620
Tanja Vinogradova <tavinogradova92@gmail.com>
2020-12-09 10:52:23 +0000

    Tanja project list skills

--- src/app/models/project.model.ts
+++ src/app/models/project.model.ts
@@ -1,12 +1,14 @@
-import { Industry } from './industry.model';
+import { ProjectSkill } from './project-skill.model';
 import { Tag } from './tag.model';
 
 export interface Project {
     id: number;
     name?: string;
     description?: string;
+    dateCreated?: Date;
+    deleted?: boolean;
     progress?: number;
-    projectSkills?: string;
+    neededSkills?: ProjectSkill[];
     industry?: string;
     tags?: Tag[];
 }
