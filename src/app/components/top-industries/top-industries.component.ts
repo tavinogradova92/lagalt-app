e2bd13d1511eb8e5befbdc592ddffd7c7a3141ca
Rune Thode <rune.thode@dk.experis.com>
2020-12-10 18:56:31 +0100

    feat: get projects from top industries

--- src/app/components/top-industries/top-industries.component.ts
+++ src/app/components/top-industries/top-industries.component.ts
@@ -1,6 +1,6 @@
 import { Industry } from './../../models/industry.model';
 import { IndustryService } from './../../services/industry.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 export class TopIndustriesComponent implements OnInit {
   industries: Industry[] = [];
   loading = true;
+  @Output() industryClicked: EventEmitter<number> = new EventEmitter();
 
   constructor(
     private router: Router,
@@ -27,9 +28,5 @@ export class TopIndustriesComponent implements OnInit {
 
   onLoad() {
     this.loading = false;
-  }
-
-  onIndustryClicked(industryId: number): void {
-    this.router.navigateByUrl(`/projects/industry/${industryId}`);
   }
 }
